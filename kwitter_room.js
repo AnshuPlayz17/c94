ce701ad0b7199f639afb77fe45fcc91c9d3b179a
AnshuPlayz17 <132763463+AnshuPlayz17@users.noreply.github.com>
2024-02-17 09:24:45 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAdF-UXz-tualTyhA7Fsk7Z8AqXZynNY3E",
+    authDomain: "lets-talk-472cd.firebaseapp.com",
+    databaseURL: "https://lets-talk-472cd-default-rtdb.firebaseio.com",
+    projectId: "lets-talk-472cd",
+    storageBucket: "lets-talk-472cd.appspot.com",
+    messagingSenderId: "509451200974",
+    appId: "1:509451200974:web:ffa62462546f3443f3e39f"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
